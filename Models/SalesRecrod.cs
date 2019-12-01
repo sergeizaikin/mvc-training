f6858bd8653995849358bfd56e62baba7ffcafba
Sergei Zaikin <sergeizaikin@hotmail.com>
2019-12-01 19:10:41 -0300

    Other Entities and second migration

---
+++ Models/SalesRecrod.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecrod
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecrod() { }
+
+        public SalesRecrod(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Salary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecrod> Sales { get; set; } = new List<SalesRecrod>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double salary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Salary = salary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecrod sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecrod sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
