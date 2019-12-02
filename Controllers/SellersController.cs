91dc784327ebe2430b500b423932b8e8900e3fc8
Sergei Zaikin <sergeizaikin@hotmail.com>
2019-12-01 22:50:52 -0300

    Simple Create form

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMVC.Controllers
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
