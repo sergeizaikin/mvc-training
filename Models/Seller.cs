2565013512c2eafc25e6045eb38fc15d003afd99
Sergei Zaikin <sergeizaikin@hotmail.com>
2019-12-03 20:46:19 -0300

    Validations

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -8,16 +8,25 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
-        public  string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [DisplayFormat(DataFormatString ="{0:dd/MM/yyy}")]
+        [Required(ErrorMessage = "{0} required")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyy}")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
 
-        [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage ="{0} must be from {1} to {2}")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         [DataType(DataType.Currency)]
         public double Salary { get; set; }
         public Department Department { get; set; }
